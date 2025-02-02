9fc2926dbad6360ccd3bfe618e7e6f9f9629848b
ashlym1 <ashlym3@usf.edu>
2025-02-02 14:40:06 -0500

    Fixed syntax and logical errors in Tasks 1-5.

--- cc_3.js
+++ cc_3.js
@@ -2,7 +2,7 @@
 let prices = [ 12.99, 5.99, 14.99, 10.49, 5.99]// Array Prices 
 prices.push(9.99); // Add new price to the end of array
 prices.shift();//Remove the first price from the array
-console.log("Updated Price List:)",prices); // Display the updates array of prices
+console.log("Updated Price List:",prices); // Display the updates array of prices
 
 // Task 2 : Modifying customer order Scenario 
 let orders = [31.97,53.96,46.46,58.94,22.47,30.37];// array orders 
@@ -19,7 +19,7 @@ performanceScore: 80,
 isActive:true
 } ; // Obeject employye
 employee.performancescore=95; //Updating the Performance Score 
-employee.promotionEliagible=true // 
+employee.promotionEligible=true // 
 console.log("Update employee Details:",employee);// display the updated employee object
 
 //Task 4: Customer FeedBack Records
@@ -30,15 +30,15 @@ let feedback =[
 ]; // Adding Feed Back Array list
 feedback.push({ customerName: "Miffy",feedbackText:"The matcha spread is so good, goes well with everything, it's my favorite !", rating: 7});
 // Add  new customer feedback to the array list to the end of the array list
-console.log("Updated feedbacks:feedback",feedback); // Display the  updated array feedbacks 
+console.log("Updated feedbacks:",feedback) // Display the  updated array feedbacks 
 
 // Task 5: Inventory Management System
 let inventory= {
-    itemName:"Ceremonial Matcha Poweder",
+    itemName:"Ceremonial Matcha Powder",
     stockCount:100,// Adjust the stock count 
     price:10.49,
 calculateTotalValue: function(){
     return this.stockCount*this.price;} 
 };//Calculating the total value 
 console.log("Inventory Details:",inventory);
-console.log("Total Inventory Value",inventory.calculateTotalValue());;// display the inventory details 
+console.log("Total Inventory Value",inventory.calculateTotalValue());// display the inventory details 
